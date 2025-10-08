7a4814ad71f06dcd04081af29ed2755e0b88a23d
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-10-08 09:20:40 +0000

    Set up /run/snapserver as fifo path

--- snapserver.nix
+++ snapserver.nix
@@ -10,16 +10,17 @@ portableService {
   inherit (snapcast) version;
   units = [
     (concatText "snapserver.service" [./snapserver.service])
+    (concatText "snapserver.socket" [./snapserver.socket])
   ];
   # required to mount StateDirectory
-  emptyDirs = ["/var/lib/snapserver"];
+  emptyDirs = ["/var/lib/snapserver" "/run/snapserver"];
   symlinks = [
     {
       object = "${snapcast}/bin/snapserver";
       symlink = "/bin/snapserver";
     }
     {
-      object = "${snapcast}/etc/snapserver.conf";
+      object = ./snapserver.conf;
       symlink = "/etc/snapserver.conf";
     }
     {

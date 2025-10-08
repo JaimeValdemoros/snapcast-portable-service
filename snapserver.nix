9854b2121c9e43e10d4f6a98b31db7e7ece53372
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-10-08 08:24:00 +0000

    Rewrite snapserver service

--- snapserver.nix
+++ snapserver.nix
@@ -9,27 +9,18 @@ portableService {
   pname = "snapserver";
   inherit (snapcast) version;
   units = [
-    (concatText "snapserver.service" ["${snapcast.src}/extras/package/rpm/snapserver.service"])
-    (writeText "snapserver-client.service" (
-      builtins.readFile "${snapcast.src}/extras/package/rpm/snapclient.service"
-      + ''
-        [Service]
-        Environment=SNAPCLIENT_OPTS="tcp://127.0.0.1:1704"
-      ''
-    ))
+    (concatText "snapserver.service" [./snapserver.service])
   ];
+  # required to mount StateDirectory
+  emptyDirs = ["/var/lib/snapserver"];
   symlinks = [
     {
       object = "${snapcast}/bin/snapserver";
-      symlink = "/usr/bin/snapserver";
+      symlink = "/bin/snapserver";
     }
     {
       object = "${snapcast}/etc/snapserver.conf";
       symlink = "/etc/snapserver.conf";
-    }
-    {
-      object = "${snapcast}/bin/snapclient";
-      symlink = "/usr/bin/snapclient";
     }
     {
       object = "${librespot}/bin/librespot";

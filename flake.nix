1daa843cbeb1aa20a6857977e9aba9d2fd68158f
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-10-08 07:03:08 +0000

    Replace flake-utils with eachSystem definition

--- flake.nix
+++ flake.nix
@@ -1,56 +1,53 @@
 {
   description = "A basic flake with a shell";
   inputs.nixpkgs.url = "github:JaimeValdemoros/nixpkgs/portable-service-empties";
-  inputs.systems.url = "github:nix-systems/default";
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.systems.follows = "systems";
-  };
+  inputs.systems.url = "github:nix-systems/default-linux";
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
+    systems,
     ...
-  }:
-    (flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [self.overlays.default];
-        };
-      in {
-        packages = {
-          default = pkgs.symlinkJoin {
-            name = "snapcast";
-            paths = [pkgs.snapserver-portable-service pkgs.snapclient-portable-service];
-          };
-          inherit (pkgs) snapserver-portable-service snapclient-portable-service;
-        };
-        formatter = pkgs.alejandra;
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bundix
-            ruby
-          ];
-        };
-      }
-    ))
-    // {
-      overlays.default = final: prev: {
-        snapserver-portable-service = final.callPackage ./snapserver.nix {};
-        snapclient-portable-service = final.callPackage ./snapclient.nix {};
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+    legacyPackages = eachSystem (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [self.overlays.default];
+      });
+  in {
+    packages = eachSystem (system: let
+      pkgs = legacyPackages.${system};
+    in {
+      default = pkgs.symlinkJoin {
+        name = "snapcast";
+        paths = [pkgs.snapserver-portable-service pkgs.snapclient-portable-service];
       };
-      nixosModules.default = {
-        pkgs,
-        config,
-        lib,
-        ...
-      }: {
-        imports = [./nixos-module.nix];
-        config = lib.mkIf config.services.pifi.enable {
-          nixpkgs.overlays = [self.overlays.default];
-        };
+      inherit (pkgs) snapserver-portable-service snapclient-portable-service;
+    });
+    formatter = eachSystem (system: legacyPackages.${system}.alejandra);
+    devShells = eachSystem (system: {
+      default = legacyPackages.${system}.mkShell {
+        packages = with legacyPackages.${system}; [
+          bundix
+          ruby
+        ];
+      };
+    });
+    overlays.default = final: prev: {
+      snapserver-portable-service = final.callPackage ./snapserver.nix {};
+      snapclient-portable-service = final.callPackage ./snapclient.nix {};
+    };
+    nixosModules.default = {
+      pkgs,
+      config,
+      lib,
+      ...
+    }: {
+      imports = [./nixos-module.nix];
+      config = lib.mkIf config.services.pifi.enable {
+        nixpkgs.overlays = [self.overlays.default];
       };
     };
+  };
 }
